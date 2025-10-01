41ad569e156ad6dff6f28849344ed833de9b047c
Serway <smaonekedwe@gmail.com>
2025-10-02 00:30:50 +0200

    fistly

---
+++ assets/js/tailwind.js
@@ -0,0 +1,14 @@
+     tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              primary: "#3b82f6",
+              secondary: "#1e40af",
+              accent: "#f59e0b",
+            },
+            fontFamily: {
+              poppins: ["Poppins", "sans-serif"],
+            },
+          },
+        },
+      };
